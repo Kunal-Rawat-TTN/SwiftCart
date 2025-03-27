ab7dea74797aba5fa2dfa093510b84f8f23e890e
Kunal-Rawat-TTN <kunal.rawat@tothenew.com>
2025-03-27 14:53:14 +0530

    Added :: Enum :: Added CustomOrderStatus enum

--- src/main/java/com/swiftcart/microservices/customEnum/CustomOrderStatus.java
+++ src/main/java/com/swiftcart/microservices/customEnum/CustomOrderStatus.java
@@ -1,5 +1,26 @@
 package com.swiftcart.microservices.customEnum;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public enum CustomOrderStatus {
+    ORDER_PLACED,
+    CANCELLED,
+    ORDER_CONFIRMED,
+    ORDER_REJECTED,
+    REFUND_INITIATED,
+    CLOSED,
+    ORDER_SHIPPED,
+    DELIVERED,
+    RETURN_REQUESTED,
+    RETURN_REJECTED,
+    RETURN_APPROVED,
+    PICK_UP_INITIATED,
+    PICK_UP_COMPLETED,
+    REFUND_COMPLETED
 
 }
